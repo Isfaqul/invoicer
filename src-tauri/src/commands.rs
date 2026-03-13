636c9c6a2895f4e968978262b2d69b9e142ec601
Isfaqul <isfaq.xperia@gmail.com>
2026-03-13 16:03:24 +0530

    Add native menus - |save|preview|quit and event handlers for the same

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,99 @@
+use rusqlite::Connection;
+use tauri::Manager;
+
+pub fn get_connection(app: &tauri::AppHandle) -> Connection {
+    let mut path = app.path().app_data_dir().unwrap();
+
+    std::fs::create_dir_all(&path).unwrap();
+
+    path.push("invoices.db");
+
+    println!("{:?}", path);
+
+    Connection::open(path).unwrap()
+}
+
+#[tauri::command]
+pub fn save_invoice(app: tauri::AppHandle, id: String, data: String) {
+    let conn = get_connection(&app);
+
+    conn.execute(
+        "INSERT OR REPLACE INTO invoices (id, data) VALUES (?1, ?2)",
+        [id, data],
+    )
+    .unwrap();
+}
+
+#[tauri::command]
+pub fn load_invoices(app: tauri::AppHandle) -> Vec<String> {
+    let conn = get_connection(&app);
+
+    let mut stmt = conn.prepare("SELECT data FROM invoices").unwrap();
+
+    stmt.query_map([], |row| row.get(0))
+        .unwrap()
+        .map(|r| r.unwrap())
+        .collect()
+}
+
+#[tauri::command]
+pub fn delete_invoice(app: tauri::AppHandle, id: String) {
+    let conn = get_connection(&app);
+
+    conn.execute("DELETE FROM invoices WHERE id = (?1)", [id])
+        .unwrap();
+}
+
+#[tauri::command]
+pub fn find_invoice(app: tauri::AppHandle, id: String) -> Option<String> {
+    let conn = get_connection(&app);
+
+    let mut stmt = conn
+        .prepare("SELECT data FROM invoices WHERE id = ?1")
+        .unwrap();
+
+    let result = stmt.query_row([id], |row| row.get(0)).ok();
+
+    result
+}
+
+#[tauri::command]
+pub fn get_last_invoice(app: tauri::AppHandle) -> Option<String> {
+    let conn = get_connection(&app);
+
+    let mut stmt = conn
+        .prepare("SELECT data FROM invoices ORDER BY rowid DESC LIMIT 1")
+        .unwrap();
+
+    let result = stmt.query_row([], |row| row.get(0)).ok();
+
+    result
+}
+
+#[tauri::command]
+pub fn get_next_invoice_id(app: tauri::AppHandle, year: i32) -> i32 {
+    let conn = get_connection(&app);
+
+    conn.execute(
+        "INSERT INTO invoice_counters (year, last_number)
+         VALUES (?1, 0)
+         ON CONFLICT(year) DO NOTHING",
+        [year],
+    )
+    .unwrap();
+
+    conn.execute(
+        "UPDATE invoice_counters
+         SET last_number = last_number + 1
+         WHERE year = ?1",
+        [year],
+    )
+    .unwrap();
+
+    conn.query_row(
+        "SELECT last_number FROM invoice_counters WHERE year = ?1",
+        [year],
+        |row| row.get(0),
+    )
+    .unwrap()
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,8 @@
-use rusqlite::Connection;
-use tauri::Manager;
+use tauri::{
+    menu::{MenuBuilder, MenuItemBuilder, SubmenuBuilder},
+    Emitter,
+};
+mod commands;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -14,34 +17,59 @@ pub fn run() {
                         .build(),
                 )?;
             }
+
             Ok(())
         })
+        .menu(|app| {
+            let save = MenuItemBuilder::with_id("save", "Save")
+                .accelerator("CmdOrCtrl+S")
+                .build(app)?;
+
+            let preview = MenuItemBuilder::with_id("preview", "Preview")
+                .accelerator("CmdOrCtrl+P")
+                .build(app)?;
+
+            let quit = MenuItemBuilder::with_id("quit", "Quit")
+                .accelerator("CmdOrCtrl+Q")
+                .build(app)?;
+
+            let file_menu = SubmenuBuilder::new(app, "File")
+                .items(&[&save, &preview, &quit])
+                .build()?;
+
+            MenuBuilder::new(app).items(&[&file_menu]).build()
+        })
+        .on_menu_event(
+            |app_handle: &tauri::AppHandle, event| match event.id().0.as_str() {
+                "save" => {
+                    println!("Save Event");
+                    let _ = app_handle.emit("menu-save", ());
+                }
+                "preview" => {
+                    println!("Preview Event");
+                    let _ = app_handle.emit("menu-preview", ());
+                }
+                "quit" => {
+                    println!("Quit Event");
+                    let _ = app_handle.emit("menu-quit", ());
+                }
+                _ => {}
+            },
+        )
         .invoke_handler(tauri::generate_handler![
-            save_invoice,
-            load_invoices,
-            delete_invoice,
-            find_invoice,
-            get_last_invoice,
-            get_next_invoice_id
+            commands::save_invoice,
+            commands::load_invoices,
+            commands::delete_invoice,
+            commands::find_invoice,
+            commands::get_last_invoice,
+            commands::get_next_invoice_id
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
-fn get_connection(app: &tauri::AppHandle) -> Connection {
-    let mut path = app.path().app_data_dir().unwrap();
-
-    std::fs::create_dir_all(&path).unwrap();
-
-    path.push("invoices.db");
-
-    println!("{:?}", path);
-
-    Connection::open(path).unwrap()
-}
-
 fn init_db(app: &tauri::AppHandle) {
-    let conn = get_connection(app);
+    let conn = commands::get_connection(app);
 
     conn.execute(
         "CREATE TABLE IF NOT EXISTS invoices (row_id INTEGER PRIMARY KEY AUTOINCREMENT, id TEXT UNIQUE NOT NULL, data TEXT NOT NULL)",
@@ -57,89 +85,4 @@ fn init_db(app: &tauri::AppHandle) {
         [],
     )
     .unwrap();
-}
-
-#[tauri::command]
-fn save_invoice(app: tauri::AppHandle, id: String, data: String) {
-    let conn = get_connection(&app);
-
-    conn.execute(
-        "INSERT OR REPLACE INTO invoices (id, data) VALUES (?1, ?2)",
-        [id, data],
-    )
-    .unwrap();
-}
-
-#[tauri::command]
-fn load_invoices(app: tauri::AppHandle) -> Vec<String> {
-    let conn = get_connection(&app);
-
-    let mut stmt = conn.prepare("SELECT data FROM invoices").unwrap();
-
-    stmt.query_map([], |row| row.get(0))
-        .unwrap()
-        .map(|r| r.unwrap())
-        .collect()
-}
-
-#[tauri::command]
-fn delete_invoice(app: tauri::AppHandle, id: String) {
-    let conn = get_connection(&app);
-
-    conn.execute("DELETE FROM invoices WHERE id = (?1)", [id])
-        .unwrap();
-}
-
-#[tauri::command]
-fn find_invoice(app: tauri::AppHandle, id: String) -> Option<String> {
-    let conn = get_connection(&app);
-
-    let mut stmt = conn
-        .prepare("SELECT data FROM invoices WHERE id = ?1")
-        .unwrap();
-
-    let result = stmt.query_row([id], |row| row.get(0)).ok();
-
-    result
-}
-
-#[tauri::command]
-fn get_last_invoice(app: tauri::AppHandle) -> Option<String> {
-    let conn = get_connection(&app);
-
-    let mut stmt = conn
-        .prepare("SELECT data FROM invoices ORDER BY rowid DESC LIMIT 1")
-        .unwrap();
-
-    let result = stmt.query_row([], |row| row.get(0)).ok();
-
-    result
-}
-
-#[tauri::command]
-fn get_next_invoice_id(app: tauri::AppHandle, year: i32) -> i32 {
-    let conn = get_connection(&app);
-
-    conn.execute(
-        "INSERT INTO invoice_counters (year, last_number)
-         VALUES (?1, 0)
-         ON CONFLICT(year) DO NOTHING",
-        [year],
-    )
-    .unwrap();
-
-    conn.execute(
-        "UPDATE invoice_counters
-         SET last_number = last_number + 1
-         WHERE year = ?1",
-        [year],
-    )
-    .unwrap();
-
-    conn.query_row(
-        "SELECT last_number FROM invoice_counters WHERE year = ?1",
-        [year],
-        |row| row.get(0),
-    )
-    .unwrap()
 }
